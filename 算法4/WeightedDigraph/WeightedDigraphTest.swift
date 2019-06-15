60b96c8c2d05dbe17877e1fbbf075b651972759b
liushan <liushan@2dfire.com>
2019-06-15 18:31:12 +0800

    优先级限制下的并行任务调度问题的关键路径方法

--- 算法4/WeightedDigraph/WeightedDigraphTest.swift
+++ 算法4/WeightedDigraph/WeightedDigraphTest.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class WeightedDigraphTest {
     static func main() {
-        AcyclicSPTest()
+        CPM()
     }
     
     static func AcyclicSPTest() {
@@ -25,8 +25,40 @@ class WeightedDigraphTest {
             print()
         }
     }
+    
+    static func CPM() {
+        let text = file(named: "jobsPC.txt")
+        var lines = text.split(separator: "\n")
+        let N = Int(lines.removeFirst())!
+        let G = EdgeWeightedDigraph(2*N+2)
+        let s = 2 * N
+        let t = s + 1
+        for i in 0..<N {
+            let a = lines.removeFirst().split(separator: " ")
+            let duration = Double(a[0])!
+            G.addEdge(DirectedEdge(from: i, to: i+N, duration))
+            G.addEdge(DirectedEdge(from: s, to: i, 0.0))
+            
+            G.addEdge(DirectedEdge(from: i+N, to: t, 0.0))
+            for j in 2..<a.count {
+                let successor = Int(a[j])!
+                G.addEdge(DirectedEdge(from: i+N, to: successor, 0.0))
+            }
+        }
+        let lp = AcyclicLP(G, s)
+        print("Start times:")
+        for i in 0..<N {
+            print("\(i.format(f: "4")): \(lp.distTo[i].format(f: "5.1"))")
+        }
+        print("Finish time: \(lp.distTo[t].format(f: "5.1"))")
+    }
 }
 
+extension Int {
+    func format(f: String) -> String {
+        return String(format: "%\(f)d", self)
+    }
+}
 extension Double {
     func format(f: String) -> String {
         return String(format: "%\(f)f", self)
